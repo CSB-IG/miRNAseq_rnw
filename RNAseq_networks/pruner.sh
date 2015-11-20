8c8a3b072f733a17fe36d9ed42e59e69265a25ed
Rodrigo García-Herrera <absorto@sdf.org>
2015-11-20 16:19:04 -0600

    somebody ought to try this

--- RNAseq_networks/pruner.sh
+++ RNAseq_networks/pruner.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
 
+# prints pruned to STDOUT
+# usage: pruner.sh THRESHOLD PRUNEE
 # prune by mi value, ej. 0.101
 # for table: node node interaction ($3 = mi value)
+THRESHOLD=$1
 
-awk -F'\t' '$3 > 0.101 {print $1"\t"$2"\t"$3}' listota.txt > listita.txt                    
+awk -F'\t' "\$3 > $THRESHOLD {print \$1'\t'\$2'\t'\$3}" $2
