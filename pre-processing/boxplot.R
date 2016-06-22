adb56035db632b74fab9bd226591de8a5c449e9d
diana <didra3@gmail.com>
2016-06-22 09:48:00 -0500

    added comments and corrected errors

--- pre-processing/boxplot.R
+++ pre-processing/boxplot.R
@@ -7,3 +7,12 @@ colnames(bp) <- c("ID", "variable", "TMM", "raw")
 pdf("TMM-raw.miRNA.753.bp.pdf",width=10,height=10)
 boxplot(bp[,3:4], data=bp)
 dev.off()
+
+
+setwd("/home/diana/")
+
+enfermos <- read.table(file = "enfermos.86.txt", header = T, sep = '\t')
+sanos <- read.table(file = "sanos.86.txt", header = T, sep = '\t')
+
+boxplot(log((sanos)+1, 2))
+boxplot(log((enfermos)+1, 2))
