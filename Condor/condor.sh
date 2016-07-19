749372fcf9a1c7726c3a16f9be900dbd5a054b10
dianadrago <didra3@gmail.com>
2016-07-19 15:33:17 -0500

    LumA redes update

--- Condor/condor.sh
+++ Condor/condor.sh
@@ -39,3 +39,20 @@ python /home/diana/parallel-aracne/genera_condor.py \
     --run_id miRNAseq_752_casos_red_Basal \
     --outdir /mnt/e/diana/TCGA_miRNA_BC/miRNAs_resultados/miRNAseq_752_casos_red_Basal \
     --p 1
+
+LumA
+python /home/diana/rnw/parallel_aracne/genera_condor.py \
+    --path_to_aracne2 /home/diana/ARACNE/aracne2 \
+    --expfile /mnt/e/diana/TCGA_miRNA_BC/miRNAs_resultados/Subtipo_LumA_Expmtx_Aracne_RNAseq.txt \
+    --probes /mnt/e/diana/TCGA_miRNA_BC/miRNAs_resultados/probes_RNAseq_752 \
+    --run_id RNAseq_752_casos_red_LumA \
+    --outdir /mnt/e/diana/TCGA_miRNA_BC/miRNAs_resultados/RNAseq_752_casos_red_LumA \
+    --p 1
+    
+python /home/diana/parallel-aracne/genera_condor.py \
+    --path_to_aracne2 /home/diana/ARACNE/aracne2 \
+    --expfile /mnt/e/diana/TCGA_miRNA_BC/miRNAs_resultados/Subtipo_LumA_Expmtx_Aracne.txt \
+    --probes /mnt/e/diana/TCGA_miRNA_BC/miRNAs_resultados/probes_miRNAs_752.txt \
+    --run_id miRNAseq_752_casos_red_LumA \
+    --outdir /mnt/e/diana/TCGA_miRNA_BC/miRNAs_resultados/miRNAseq_752_casos_red_LumA \
+    --p 1
