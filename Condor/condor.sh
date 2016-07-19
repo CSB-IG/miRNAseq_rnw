c4feae06a94a08257fa676e5a003b0384410fa48
dianadrago <didra3@gmail.com>
2016-07-19 12:45:54 -0500

    Update condor.sh

--- Condor/condor.sh
+++ Condor/condor.sh
@@ -22,3 +22,20 @@ python /home/diana/breast_cancer_networks/parallel_aracne/genera_condor.py \
 
 sanos <- "sanos.86.txt"
 probes sanos <- "GENEID.86.txt"
+
+Basal
+python /home/diana/rnw/parallel_aracne/genera_condor.py \
+    --path_to_aracne2 /home/diana/ARACNE/aracne2 \
+    --expfile /mnt/e/diana/TCGA_miRNA_BC/miRNAs_resultados/Subtipo_Basal_Expmtx_Aracne_RNAseq.txt \
+    --probes /mnt/e/diana/TCGA_miRNA_BC/miRNAs_resultados/probes_RNAseq_752 \
+    --run_id RNAseq_752_casos_red_Basal \
+    --outdir /mnt/e/diana/TCGA_miRNA_BC/miRNAs_resultados/RNAseq_752_casos_red_Basal \
+    --p 1
+    
+python /home/diana/parallel-aracne/genera_condor.py \
+    --path_to_aracne2 /home/diana/ARACNE/aracne2 \
+    --expfile /mnt/e/diana/TCGA_miRNA_BC/miRNAs_resultados/Subtipo_Basal_Expmtx_Aracne.txt \
+    --probes /mnt/e/diana/TCGA_miRNA_BC/miRNAs_resultados/probes_miRNAs_752.txt \
+    --run_id miRNAseq_752_casos_red_Basal \
+    --outdir /mnt/e/diana/TCGA_miRNA_BC/miRNAs_resultados/miRNAseq_752_casos_red_Basal \
+    --p 1
