44455aac388495e650334f67ac08330eb7a411ce
dianadrago <diana_drago@live.com>
2016-08-25 13:20:33 -0500

    plot everything, strange colors

--- hiveplot/hiveplot.py
+++ hiveplot/hiveplot.py
@@ -2,8 +2,18 @@ from pyveplot import *
 import networkx as nx
 import csv
 import argparse
+import math
+
+
+def coords( radius, angle, origin=(0,0)):
+    """ Returns a tuple of tuples of coordinates given a radius, angle and origin tuple """
+    return (origin[0] + round((radius * math.cos(math.radians(angle))), 2),
+            origin[1] + round((radius * math.sin(math.radians(angle))), 2))
+
 
 from pprint import pprint
+
+
 
 parser = argparse.ArgumentParser(description='hiveplot of mirna-gene interactions')
 parser.add_argument('edgelist', type=argparse.FileType('r'), help="network in edgelist format")
@@ -29,7 +39,7 @@ for n in g.nodes():
         by_degree[d] = [n, ]
 
 degree_ordered = []
-for d in sorted(by_degree.keys(), reverse=True):
+for d in sorted(by_degree.keys(), reverse=False):
     degree_ordered += by_degree[d]
 
 
@@ -50,10 +60,9 @@ mirnas      = []
 # classify nodes
 for n in degree_ordered:
     if not n.startswith('hsa'):
+        genes.append(n)        
         if only_talks_to_mrna(n):
             mirna_genes.append(n)
-        else:
-            genes.append(n)
     else:
         mirnas.append(n)
 
@@ -63,30 +72,74 @@ print len(mirnas),len(genes),len(mirna_genes)
 
 h = Hiveplot( args.plot.name )
 args.plot.close()
+h.dwg.size=(5000,5000)
 
-a = Axis( (200,200), (200,100), stroke="grey" )
-b = Axis( (250,200), (250,100), stroke="blue" )
+centre = (750, 250)
 
-h.axes = [a, b]
 
+# configure mirna axes
+m1 = Axis( centre, coords(250, -120, centre), stroke="red", stroke_width=4)
+m2 = Axis( centre, coords(250, -60, centre), stroke="red", stroke_width=4)
 pos   = 0.0
 delta = 1.0 / len(mirnas)
 for n in mirnas:
     node0 = Node(n)    
     node1 = Node(n)
-    a.add_node( node0, pos ) 
-    b.add_node( node1, pos )
+    m1.add_node( node0, pos ) 
+    m2.add_node( node1, pos )
     pos += delta
 
+
+
+ga = Axis(centre, coords(750, 30, centre), stroke="orange", stroke_width=4)
+mg = Axis(centre, coords(750, 150, centre), stroke="red", stroke_width=4)
+
+
+pos   = 0.0
+delta = 1.0 / len(genes)
+for n in genes:
+    node0 = Node(n)
+    node1 = Node(n) 
+    ga.add_node( node0, pos ) 
+    mg.add_node( node1, pos )
+    pos += delta
+
+h.axes = [m1, m2, ga, mg]
+
+    
 for e in g.edges():
+    # mirnas to mirnas
     if e[0] in mirnas and e[1] in mirnas:
-        h.connect(a, e[0], 5,
-                  b, e[1], -5,
+        h.connect(m1, e[0], 25,
+                  m2, e[1], -25,
                   stroke_width='0.34', stroke_opacity='0.4', stroke='purple')
-    # if e[0] in axis_mrna1.nodes and e[1] in axis_mrna0.nodes:
-    #     h.connect(axis_mrna1, e[1], 20,
-    #               axis_mrna0, e[0], -20,
-    #               stroke_width='0.34', stroke_opacity='0.4', stroke='purple')
+
+
+    if e[0] in mirnas and e[1] in mirna_genes: # mirnas to mirna-genes
+        h.connect(m1, e[0], -25,
+                  mg, e[1], 25,
+                  stroke_width='0.34', stroke_opacity='0.4', stroke='blue')
+    elif e[0] in mirnas and e[1] in genes: # mirnas to genes
+        h.connect(m2, e[0], 25,
+                  ga, e[1], -25,
+                  stroke_width='0.34', stroke_opacity='0.4', stroke='green')
+
+
+    if e[1] in mirnas and e[0] in mirna_genes: # mirna-genes to mirnas
+        h.connect(m1, e[1], -25,
+                  mg, e[0], 25,
+                  stroke_width='0.34', stroke_opacity='0.4', stroke='blue')
+    elif e[1] in mirnas and e[0] in mirna_genes: # genes to mirnas
+        h.connect(m2, e[1], 25,
+                  ga, e[0], -25,
+                  stroke_width='0.34', stroke_opacity='0.4', stroke='green')
+
+
+    if e[0] in genes and e[1] in genes or e[1] in genes and e[0] in genes: # gene to gene
+        h.connect(ga, e[0], 25,
+                  mg, e[1], -25,
+                  stroke_width='0.34', stroke_opacity='0.4', stroke='yellow')
+
         
 h.save()
 
