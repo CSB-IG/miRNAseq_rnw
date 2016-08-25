5ae89e473f8e197c953175de86376995bc48cb46
dianadrago <diana_drago@live.com>
2016-08-25 16:40:26 -0500

    added progressbar, different angles and colors

--- hiveplot/hiveplot.py
+++ hiveplot/hiveplot.py
@@ -3,7 +3,7 @@ import networkx as nx
 import csv
 import argparse
 import math
-
+import progressbar
 
 def coords( radius, angle, origin=(0,0)):
     """ Returns a tuple of tuples of coordinates given a radius, angle and origin tuple """
@@ -26,7 +26,7 @@ args   = parser.parse_args()
 # load edges into graph
 g = nx.Graph()
 for edge in csv.DictReader(args.edgelist, delimiter="\t"):
-    g.add_edge( edge['from'], edge['to'], w=edge['weight'] )
+    g.add_edge( edge['from'], edge['to'], w=float(edge['weight']) )
 
 
 # sort nodes by degree
@@ -72,14 +72,15 @@ print len(mirnas),len(genes),len(mirna_genes)
 
 h = Hiveplot( args.plot.name )
 args.plot.close()
-h.dwg.size=(5000,5000)
+h.dwg.width=15000
+h.dwg.height=15000
 
-centre = (750, 250)
+centre = (750, 500)
 
 
 # configure mirna axes
-m1 = Axis( centre, coords(250, -120, centre), stroke="red", stroke_width=4)
-m2 = Axis( centre, coords(250, -60, centre), stroke="red", stroke_width=4)
+m1 = Axis( coords(70, -100, centre), coords(350, -120, centre), stroke="maroon", stroke_width=4)
+m2 = Axis( coords(70, -80, centre), coords(350, -60, centre), stroke="maroon", stroke_width=4)
 pos   = 0.0
 delta = 1.0 / len(mirnas)
 for n in mirnas:
@@ -91,8 +92,8 @@ for n in mirnas:
 
 
 
-ga = Axis(centre, coords(750, 30, centre), stroke="orange", stroke_width=4)
-mg = Axis(centre, coords(750, 150, centre), stroke="red", stroke_width=4)
+ga = Axis(coords(50, 45, centre), coords(750, 45, centre), stroke="dodgerblue", stroke_width=4)
+mg = Axis(coords(50, 135, centre), coords(750, 135, centre), stroke="dodgerblue", stroke_width=4)
 
 
 pos   = 0.0
@@ -106,43 +107,53 @@ for n in genes:
 
 h.axes = [m1, m2, ga, mg]
 
-    
-for e in g.edges():
-    # mirnas to mirnas
-    if e[0] in mirnas and e[1] in mirnas:
+
+bar = progressbar.ProgressBar()
+
+for e in bar(g.edges()):
+
+    if e[0] in mirnas and e[1] in mirnas:     # mirnas to mirnas
         h.connect(m1, e[0], 25,
                   m2, e[1], -25,
-                  stroke_width='0.34', stroke_opacity='0.4', stroke='purple')
+                  stroke_width   = g.get_edge_data(*e)['w'] * 2,
+                  stroke_opacity = 0.5,
+                  stroke         = 'purple')
 
 
     if e[0] in mirnas and e[1] in mirna_genes: # mirnas to mirna-genes
-        h.connect(m1, e[0], -25,
-                  mg, e[1], 25,
-                  stroke_width='0.34', stroke_opacity='0.4', stroke='blue')
+        h.connect(m1, e[0], -45,
+                  mg, e[1], 45,
+                  stroke_width   = g.get_edge_data(*e)['w'] * 2,
+                  stroke_opacity = 0.15,
+                  stroke         = 'slategray')
     elif e[0] in mirnas and e[1] in genes: # mirnas to genes
-        h.connect(m2, e[0], 25,
-                  ga, e[1], -25,
-                  stroke_width='0.34', stroke_opacity='0.4', stroke='green')
+        h.connect(m2, e[0], 45,
+                  ga, e[1], -45,
+                  stroke_width=g.get_edge_data(*e)['w'] * 2,
+                  stroke_opacity=0.15,
+                  stroke='navajowhite')
 
 
     if e[1] in mirnas and e[0] in mirna_genes: # mirna-genes to mirnas
-        h.connect(m1, e[1], -25,
-                  mg, e[0], 25,
-                  stroke_width='0.34', stroke_opacity='0.4', stroke='blue')
+        h.connect(m1, e[1], -45,
+                  mg, e[0], 45,
+                  stroke_width=g.get_edge_data(*e)['w'] * 2,
+                  stroke_opacity=0.15,
+                  stroke='slategray')
     elif e[1] in mirnas and e[0] in mirna_genes: # genes to mirnas
-        h.connect(m2, e[1], 25,
-                  ga, e[0], -25,
-                  stroke_width='0.34', stroke_opacity='0.4', stroke='green')
+        h.connect(m2, e[1], 45,
+                  ga, e[0], -45,
+                  stroke_width=g.get_edge_data(*e)['w'] * 2,
+                  stroke_opacity=0.15,
+                  stroke='navajowhite')
 
 
     if e[0] in genes and e[1] in genes or e[1] in genes and e[0] in genes: # gene to gene
-        h.connect(ga, e[0], 25,
-                  mg, e[1], -25,
-                  stroke_width='0.34', stroke_opacity='0.4', stroke='yellow')
+        h.connect(ga, e[0], 33,
+                  mg, e[1], -33,
+                  stroke_width=g.get_edge_data(*e)['w'] * 2,
+                  stroke_opacity=0.15,
+                  stroke='gainsboro')
 
-        
+print "saving"        
 h.save()
-
-# nx.write_gpickle(g, "test.gpickle")
-
-#pprint(mirna_genes)
