94b10f68733dc156b68e6253920b61a4ee2cdfea
dianadrago <diana_drago@live.com>
2016-09-08 16:24:28 -0500

    use mirna positions instead of hsa positions

--- circos_plots/sort_links.py
+++ circos_plots/sort_links.py
@@ -1,8 +1,8 @@
 import argparse
 import csv
-from hsa_positions import position as hsa_pos
-from mimat_positions import positions as mimat_pos
+
 from gene_positions import positions as gene_pos
+from mirna_pos import mirna_pos
 
 parser = argparse.ArgumentParser(description='Create a links file for circos plot from edgelist.')
 parser.add_argument('edgelist', type=argparse.FileType('r'), help="path to edgelist" )
@@ -10,11 +10,10 @@ args = parser.parse_args()
 
 reader = csv.DictReader(args.edgelist, delimiter="\t")
 
-
 def node2pos(node):
     if node.startswith('hsa-'):
         (hsa, mimat) = node.split('.')
-        return hsa_pos.get(hsa, mimat_pos.get(mimat))
+        return mirna_pos.get(mimat, "AGUAS")
     else:
         return gene_pos.get(node)
 
